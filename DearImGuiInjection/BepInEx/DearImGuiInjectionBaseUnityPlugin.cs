8f0e86351f34315c90ca9fc9bd530f2ec759408a
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-06-09 22:54:53 +0200

    feature(ui): ignore ui objects when imgui cursor is visible

--- DearImGuiInjection/BepInEx/DearImGuiInjectionBaseUnityPlugin.cs
+++ DearImGuiInjection/BepInEx/DearImGuiInjectionBaseUnityPlugin.cs
@@ -1,12 +1,20 @@
+using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using BepInEx;
 using DearImGuiInjection.Backends;
+using MonoMod.RuntimeDetour;
 
 namespace DearImGuiInjection.BepInEx;
 
 [BepInPlugin(Metadata.GUID, Metadata.Name, Metadata.Version)]
 internal class DearImGuiInjectionBaseUnityPlugin : BaseUnityPlugin
 {
+    private Type _eventSystemType;
+    private MethodInfo _eventSystemUpdate;
+    private Hook _eventSystemUpdateHook;
+
     private void Awake()
     {
         Log.Init(new BepInExLog(Logger));
@@ -21,11 +29,41 @@ internal class DearImGuiInjectionBaseUnityPlugin : BaseUnityPlugin
             "Key for switching the cursor visibility."));
         DearImGuiInjection.Init(imguiIniConfigDirectoryPath, assetsFolder, cursorVisibilityConfig);
 
+        SetupIgnoreUIObjectsWhenImGuiCursorIsVisible();
+
         gameObject.AddComponent<UnityMainThreadDispatcher>();
+    }
+
+    private void SetupIgnoreUIObjectsWhenImGuiCursorIsVisible()
+    {
+        try
+        {
+            var allFlags = (BindingFlags)(-1);
+            var unityEngineUIDll = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(ass => ass.GetName().Name == "UnityEngine.UI");
+            _eventSystemType = unityEngineUIDll.GetType("UnityEngine.EventSystems.EventSystem");
+            _eventSystemUpdate = _eventSystemType.GetMethod("Update", allFlags);
+            _eventSystemUpdateHook = new Hook(_eventSystemUpdate, IgnoreUIObjectsWhenImGuiCursorIsVisible);
+        }
+        catch (Exception e)
+        {
+            Log.Error(e);
+        }
+    }
+
+    private static void IgnoreUIObjectsWhenImGuiCursorIsVisible(Action<object> orig, object self)
+    {
+        if (DearImGuiInjection.IsCursorVisible)
+        {
+            return;
+        }
+
+        orig(self);
     }
 
     private void OnDestroy()
     {
+        _eventSystemUpdateHook?.Dispose();
+
         DearImGuiInjection.Dispose();
     }
 }
