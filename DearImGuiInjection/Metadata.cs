4daaa1556d33094dafb2ec69090a6444bd304b7c
Quentin E. / iDeath <xiaoxiao921@hotmail.fr>
2023-06-09 17:22:46 +0200

    typo

--- DearImGuiInjection/Metadata.cs
+++ DearImGuiInjection/Metadata.cs
@@ -4,6 +4,6 @@ public static class Metadata
 {
     public const string GUID = Author + "." + Name;
     public const string Author = "iDeathHD";
-    public const string Name = "DearImguiInjection";
+    public const string Name = "DearImGuiInjection";
     public const string Version = "1.0.0";
 }
